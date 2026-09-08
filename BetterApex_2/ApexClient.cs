008cea590053bd4b1106a61f3228b84e7b793293
HaBalint <balintcsakjatekra@gamil.com>
2026-09-08 15:44:47 +0200

    Add project files.

---
+++ BetterApex_2/ApexClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace BetterApex
+{
+    public class ApexWebSocketClient
+    {
+        private readonly Uri _serverUri;
+        private WebsocketClient _client;
+
+        public ApexWebSocketClient()
+        {
+            _serverUri = new Uri("wss://live-data.apex-timing.com:8533/");
+        }
+
+        public async Task ConnectAsync()
+        {
+            _client = new WebsocketClient(_serverUri);
+
+            _client.ReconnectionHappened.Subscribe(info =>
+            {
+                Debug.WriteLine($"----------------------------------------------------------------------- WS connected: {info.Type} -----------------------------------------------------------------------");
+            });
+
+            _client.DisconnectionHappened.Subscribe(info =>
+            {
+                Debug.WriteLine($"----------------------------------------------------------------------- WS disconnected: {info.Type} -----------------------------------------------------------------------");
+            });
+
+            _client.MessageReceived.Subscribe(msg =>
+            {
+                Debug.WriteLine(msg.Text);
+            });
+
+            await _client.Start();
+        }
+    }
+}
---
+++ BetterApex_2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BetterApex;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BetterApex_2
+{
+    public partial class MainWindow : Window
+    {
+        private ApexWebSocketClient _apexClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _apexClient = new ApexWebSocketClient();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _apexClient.ConnectAsync();
+        }
+    }
+}
