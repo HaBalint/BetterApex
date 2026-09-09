02ab792b6664dbe385da8dc3fdeb662e97b17048
Balint <balint.hadlaczki@gmail.com>
2026-09-09 18:59:01 +0200

    Kultúráltabb adatfeldolgozás

--- BetterApex_2/InitialParser.cs
+++ BetterApex_2/InitialParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using HtmlAgilityPack;
 
 namespace BetterApex_2
 {
@@ -11,19 +12,41 @@ namespace BetterApex_2
         {
             int gridStart = rawMessage.IndexOf("grid||");   // van e benne egyáltalán grid info (ha nincs akkor -1 az értéke)
             int htmlStart=rawMessage.IndexOf("<tbody>");    // van e benne egyáltalán <tbody> (ha nincs akkor -1 az értéke)
-            if (gridStart == -1 || htmlStart == -1)
+            if (gridStart == -1 || htmlStart == -1)         //ha nincs grid info vagy nincs <tbody> akkor nem kell tovább menni
             {
                 return;
             }
             int gridEnd = rawMessage.IndexOf("</tbody>");
             int htmlLength = gridEnd+"</tbody>".Length-htmlStart;
-            string gridHtml = rawMessage.Substring(htmlStart, htmlLength);
+            
             Debug.WriteLine($"Grid Start: {gridStart}");
             Debug.WriteLine("Grid end: " + gridEnd);
             Debug.WriteLine("HTML Start: " + htmlStart);
             Debug.WriteLine("HTML Length: " + htmlLength);
-            Debug.WriteLine("Grid HTML: " + gridHtml);
-           
+
+            string gridHtml = rawMessage.Substring(htmlStart, htmlLength);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(gridHtml);
+
+            var rows = doc.DocumentNode.SelectNodes("//tr");
+
+            Debug.WriteLine($"Rows found: {rows?.Count}");
+            foreach (var row in rows)
+            {
+                string rowId = row.GetAttributeValue("data-id", "");
+
+                if (rowId == "r0")
+                {
+                    continue;                           //greek mode (NOP)
+                }
+
+                var kartCell = row.SelectSingleNode($".//*[@data-id='{rowId}c5']");
+                string kartNumber = kartCell?.InnerText ?? "";
+
+                Debug.WriteLine($"Entry row: {rowId}, Kart: {kartNumber}");
+            }
+
         }
     }
 }
