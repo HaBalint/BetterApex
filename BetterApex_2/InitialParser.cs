bbeb507c53171187533a6ec4100358ca16990774
Balint <balint.hadlaczki@gmail.com>
2026-09-09 19:10:24 +0200

    Üres race state hozzáadva

--- BetterApex_2/InitialParser.cs
+++ BetterApex_2/InitialParser.cs
@@ -10,26 +10,26 @@ namespace BetterApex_2
     {
         public void Parse(string rawMessage)
         {
-            int gridStart = rawMessage.IndexOf("grid||");   // van e benne egyáltalán grid info (ha nincs akkor -1 az értéke)
-            int htmlStart=rawMessage.IndexOf("<tbody>");    // van e benne egyáltalán <tbody> (ha nincs akkor -1 az értéke)
-            if (gridStart == -1 || htmlStart == -1)         //ha nincs grid info vagy nincs <tbody> akkor nem kell tovább menni
+            int gridStart = rawMessage.IndexOf("grid||");                       // van e benne egyáltalán grid info (ha nincs akkor -1 az értéke)
+            int htmlStart=rawMessage.IndexOf("<tbody>");                        // van e benne egyáltalán <tbody> (ha nincs akkor -1 az értéke)
+            if (gridStart == -1 || htmlStart == -1)                             //ha nincs grid info vagy nincs <tbody> akkor nem kell tovább menni
             {
                 return;
             }
             int gridEnd = rawMessage.IndexOf("</tbody>");
             int htmlLength = gridEnd+"</tbody>".Length-htmlStart;
-            
+            //-------------------szarságok magamnak-------------------
             Debug.WriteLine($"Grid Start: {gridStart}");
             Debug.WriteLine("Grid end: " + gridEnd);
             Debug.WriteLine("HTML Start: " + htmlStart);
             Debug.WriteLine("HTML Length: " + htmlLength);
-
+            //--------------------------------------------------------
             string gridHtml = rawMessage.Substring(htmlStart, htmlLength);
 
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(gridHtml);
+            HtmlDocument body_html = new HtmlDocument();                        //load gridhtml as html document
+            body_html.LoadHtml(gridHtml);
 
-            var rows = doc.DocumentNode.SelectNodes("//tr");
+            var rows = body_html.DocumentNode.SelectNodes("//tr");              //table rows keresés
 
             Debug.WriteLine($"Rows found: {rows?.Count}");
             foreach (var row in rows)
@@ -38,7 +38,7 @@ namespace BetterApex_2
 
                 if (rowId == "r0")
                 {
-                    continue;                           //greek mode (NOP)
+                    continue;                                                   //greek mode (NOP)
                 }
 
                 var kartCell = row.SelectSingleNode($".//*[@data-id='{rowId}c5']");
